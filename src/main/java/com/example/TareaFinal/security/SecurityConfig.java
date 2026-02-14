8ecba6f265bde11512cc7259562b86b6bbf566e8
Eleil24 <eleil65723@gmail.com>
2026-02-14 18:04:52 -0500

    Levantar en render

--- src/main/java/com/example/TareaFinal/security/SecurityConfig.java
+++ src/main/java/com/example/TareaFinal/security/SecurityConfig.java
@@ -71,12 +71,19 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://proyect-movie-crud.vercel.app"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://proyect-movie-crud.vercel.app"
+        ));
+
+        configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 
